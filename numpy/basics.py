4a49747d45bb22722fd692a1a7dcd32ce495f8d7
Partha <parthasdeyofficial@gmail.com>
2024-02-02 21:55:07 +0530

    initial commit

--- numpy/basics.py
+++ numpy/basics.py
@@ -11,4 +11,8 @@ print(arr_1.shape) # Output: (5,)
 print(arr_2.shape) # Output: (2, 3)
 print(arr_2.dtype) # Prints the data types of the arrays Output: int64
 print(arr_2.ndim) # Prints the number of dimensions of the axes
+print(arr_1.size) # printing the size of the first array
+print(arr_2.size) # printing the size of the second array
+print(arr_1.itemsize) # printing the number of bytes of the array elements of the first array
+print(arr_2.itemsize) # printing the number of bytes of the array elements of the second array
 
