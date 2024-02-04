e1dea3a9e3734795c325fa7a5efecc147b7b43c6
Partha <parthasdeyofficial@gmail.com>
2024-02-04 12:16:44 +0530

    initial commit

---
+++ numpy/q10.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+arr = np.linspace(0, 1, 10) # this generates 10 equally spaces points in between 0 to 1
+
+print("This array contains 10 equally spaced numbers between 0 to 1 : \n", arr)
---
+++ numpy/q11.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr = np.arange(1, 11)
+arr_sum = np.sum(arr)
+
+print("The sum of all the elements from 1 to 10 are : ", arr_sum)
